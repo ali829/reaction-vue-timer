fda5ed9f7d48590a09f2016ca96ec761222c916a
ali elyassraoui <alielyassraoui@gmai.com>
2021-06-08 16:22:24 +0100

    final rouches of block component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>reaction timer</h1>
+    <button @click="play" >play</button>
+    <block v-if="isPlay" :delay="delay" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import block from "./components/block.vue";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {block},
+  data: ()=> ({
+      isPlay : false,
+      delay : null
+  }),
+  methods : {
+    play() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlay = true;
+    }
   }
 }
 </script>
@@ -20,7 +28,18 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #4444;
   margin-top: 60px;
+}
+button{
+  padding:5px 20px;
+  border-radius: 4px;
+  color: azure;
+  appearance: none;
+  border: none;
+  background-color: cadetblue;
+  outline: none;
+  cursor: pointer;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);;
 }
 </style>
---
+++ src/components/block.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="block" v-if="showBlock" @click="endTimer">
+        <p>Click me</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "block",
+        props: {
+            delay: {
+                type: Number,
+                default: null,
+                required: true
+            },
+        },
+        data() {
+            return {
+                showBlock: false,
+                timer: null,
+                timeReaction: 0
+            }
+        },
+        mounted() {
+            setTimeout(() => {
+                this.showBlock = true;
+                this.startTimer();
+            }, this.delay);
+        },
+        methods:{
+            startTimer(){
+                this.timer = setInterval(()=>{
+                    this.timeReaction += 10;
+                } , 10)
+            },
+            endTimer(){
+                clearInterval(this.timer);
+            }
+        }
+
+
+    }
+</script>
+
+<style>
+    .block {
+        width: 400px;
+        padding: 100px 0;
+        margin: 40px auto;
+        background-color: cadetblue;
+        border-radius: 10px;
+    }
+</style>
